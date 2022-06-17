e1269f771327c7939fd6bccf610db8081694183c
Juliana Ribeiro <julianaribeiro0811@gmail.com>
2022-06-16 23:07:43 -0400

    “mensagem”

---
+++ src/Title.jsx
@@ -0,0 +1,12 @@
+import "./Novo.css"
+function Title(){
+    return(
+       < div>
+        
+            <p className="title">    NOTÍCIAS</p>
+            <p className="subtitle">PORTAL DO IFRO</p>
+       
+       </div>
+    )
+}
+export default Title;
